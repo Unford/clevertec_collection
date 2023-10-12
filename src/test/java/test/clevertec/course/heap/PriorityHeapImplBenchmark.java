a03277f546fb2cc3c2de5fda84b337d56a2ff90a
vladimir_feshenko <vladimir0feshenko@gmail.com>
2023-10-12 18:19:10 +0300

    benchmark and tests

---
+++ src/test/java/test/clevertec/course/heap/PriorityHeapImplBenchmark.java
@@ -0,0 +1,92 @@
+package test.clevertec.course.heap;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import ru.clevertec.course.heap.impl.PriorityHeapImpl;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class PriorityHeapImplBenchmark {
+    private static final int ITERATION_COUNT = 10000;
+    private PriorityHeapImpl<Integer> globalHeap;
+    private PriorityQueue<Integer> globalQueue;
+
+    @Setup(Level.Invocation)
+    public void setupInvokation() throws Exception {
+        globalHeap = new PriorityHeapImpl<>();
+        globalQueue = new PriorityQueue<>();
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            globalHeap.add(i);
+            globalQueue.add(i);
+
+        }
+    }
+
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 3)
+    @Measurement(iterations = 3)
+    @Fork(value = 1, warmups = 1)
+    public boolean benchmarkStandardPriorityQueueAdd(Blackhole blackhole) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            priorityQueue.add(i);
+        }
+        boolean res = priorityQueue.size() == ITERATION_COUNT;
+        blackhole.consume(res);
+        return res;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 3)
+    @Measurement(iterations = 3)
+    @Fork(value = 1, warmups = 1)
+    public boolean benchmarkCustomPriorityQueueAdd(Blackhole blackhole) {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>(Comparator.reverseOrder());
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            priorityHeap.add(i);
+        }
+        boolean res = priorityHeap.size() == ITERATION_COUNT;
+        blackhole.consume(res);
+        return res;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 3)
+    @Measurement(iterations = 3)
+    @Fork(value = 1, warmups = 1)
+    public boolean benchmarkStandardPriorityQueuePoll(Blackhole blackhole) {
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            globalQueue.poll();
+        }
+        boolean res = globalQueue.isEmpty();
+        blackhole.consume(res);
+        return res;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 3)
+    @Measurement(iterations = 3)
+    @Fork(value = 1, warmups = 1)
+    public boolean benchmarkCustomPriorityQueuePoll(Blackhole blackhole) {
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            globalHeap.poll();
+        }
+        boolean res = globalHeap.isEmpty();
+        blackhole.consume(res);
+        return res;
+    }
+
+}
---
+++ src/test/java/test/clevertec/course/heap/PriorityHeapImplTest.java
@@ -0,0 +1,223 @@
+package test.clevertec.course.heap;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.clevertec.course.heap.impl.PriorityHeapImpl;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriorityHeapImplTest {
+    private class TestNotComparable {
+        private int id;
+    }
+
+    private static class TestComparable implements Comparable<TestComparable> {
+        private int id;
+        private double price;
+
+
+        public TestComparable(int id, double price) {
+            this.id = id;
+            this.price = price;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        @Override
+        public int compareTo(TestComparable o) {
+            return Comparator.comparingInt(TestComparable::getId)
+                    .thenComparingDouble(TestComparable::getPrice).compare(this, o);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof TestComparable that)) return false;
+
+            if (id != that.id) return false;
+            return Double.compare(price, that.price) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            result = id;
+            temp = Double.doubleToLongBits(price);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            return result;
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("IntegerPriorityHeapDataProvider")
+    void givenList_whenPoll_thenReturnMin(List<Integer> integers) {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>();
+        priorityHeap.addAll(integers);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(integers);
+        Integer actual = priorityHeap.poll();
+        Integer expected = priorityQueue.poll();
+
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> IntegerPriorityHeapDataProvider() {
+        return Stream.of(
+                Arguments.of(List.of(4, 2, 6, 12, 9, 2, 61, 1)),
+                Arguments.of(List.of(12, 822, 2, 143, 1, 32, 61, 111)),
+                Arguments.of(List.of(-4, 0, -12, 461, 3, 32, 135, 1)),
+                Arguments.of(List.of(42)));
+    }
+
+
+//    @Test todo
+//    void givenNotComparableClassWithoutComparator_whenCreateAndAdd_thenThrow() {
+//        assertThrows(ClassCastException.class,
+//                () -> new PriorityHeapImpl<TestNotComparable>()
+//                        .add(new TestNotComparable()));
+//    }
+
+    @Test
+    void givenNull_whenAdd_thenThrow() {
+        assertThrows(NullPointerException.class,
+                () -> new PriorityHeapImpl<TestNotComparable>().add(null));
+    }
+
+    @Test
+    void givenEmptyHeap_whenPeekOrPoll_thenNull() {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>();
+        assertNull(priorityHeap.peek());
+        assertNull(priorityHeap.poll());
+
+    }
+
+    @Test
+    void givenComparableClassWithComparator_whenCreate_thenPeek() {
+        TestComparable expected = new TestComparable(10, 0.0);
+        PriorityHeapImpl<TestComparable> priorityHeap = new PriorityHeapImpl<>();
+        priorityHeap.add(expected);
+        TestComparable actual = priorityHeap.peek();
+        assertEquals(expected, actual);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("IntegerAndComparatorPriorityHeapDataProvider")
+    void givenListAndComparator_whenPoll_thenReturn(List<Integer> integers,
+                                                    Comparator<Integer> comparator) {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>(comparator);
+        priorityHeap.addAll(integers);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(comparator);
+        priorityQueue.addAll(integers);
+        Integer actual = priorityHeap.poll();
+        Integer expected = priorityQueue.poll();
+
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> IntegerAndComparatorPriorityHeapDataProvider() {
+        return Stream.of(
+                Arguments.of(List.of(4, 2, 6, 12, 9, 2, 61, 1), Comparator.naturalOrder()),
+                Arguments.of(List.of(12, 822, 2, 143, 1, 32, 61, 111), Comparator.reverseOrder()),
+                Arguments.of(List.of(-4, 0, -12, 461, 3, 32, 135, 1),
+                        Comparator.comparingInt(value -> ((Integer) value) % 2)),
+                Arguments.of(List.of(42), null));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("IntegerPriorityHeapDataProvider")
+    void givenList_whenPollAll_thenReturnNull(List<Integer> integers) {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>();
+        priorityHeap.addAll(integers);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.addAll(integers);
+        while (!priorityHeap.isEmpty()) {
+            priorityHeap.poll();
+            priorityQueue.poll();
+            assertThat(priorityHeap).containsExactlyElementsOf(priorityQueue);
+        }
+
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("ComparableAndComparatorPriorityHeapDataProvider")
+    void givenComparableListAndComparator_whenPoll_thenReturn(List<TestComparable> comparableList,
+                                                              Comparator<TestComparable> comparator) {
+        PriorityHeapImpl<TestComparable> priorityHeap = new PriorityHeapImpl<>(comparator);
+        priorityHeap.addAll(comparableList);
+
+        PriorityQueue<TestComparable> priorityQueue = new PriorityQueue<>(comparator);
+        priorityQueue.addAll(comparableList);
+        TestComparable actual = priorityHeap.poll();
+        TestComparable expected = priorityQueue.poll();
+        assertEquals(expected, actual);
+
+    }
+
+    static Stream<Arguments> ComparableAndComparatorPriorityHeapDataProvider() {
+        return Stream.of(
+                Arguments.of(getTestComparableList(), null),
+                Arguments.of(getTestComparableList(), Comparator.reverseOrder()),
+                Arguments.of(getTestComparableList(), Comparator.naturalOrder()),
+                Arguments.of(getTestComparableList(), Comparator.comparingDouble(TestComparable::getPrice)),
+                Arguments.of(getTestComparableList(),
+                        Comparator.comparingDouble(TestComparable::getPrice).thenComparingInt(TestComparable::getId)),
+                Arguments.of(getTestComparableList(), Comparator.comparingInt(TestComparable::getId)),
+                Arguments.of(List.of(new TestComparable(1, 1.1)), null));
+    }
+
+    private static List<TestComparable> getTestComparableList() {
+        return List.of(new TestComparable(12, 11.4),
+                new TestComparable(-3, 11.4),
+                new TestComparable(12, 121.4),
+                new TestComparable(342, 98.92),
+                new TestComparable(5, 24.2),
+                new TestComparable(12, -3.3),
+                new TestComparable(93, -3.3),
+                new TestComparable(-6, 2.4),
+                new TestComparable(6, 932.4));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("IntegerListWithListToRemovePriorityHeapDataProvider")
+    void givenIntegerListAndRemoveList_whenRemove_thenReturn(List<Integer> integers, List<Integer> toRemove) {
+        PriorityHeapImpl<Integer> priorityHeap = new PriorityHeapImpl<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.addAll(integers);
+        priorityHeap.addAll(integers);
+        toRemove.forEach(r -> {
+            priorityHeap.remove(r);
+            priorityQueue.remove(r);
+        });
+
+
+        assertThat(priorityHeap).containsExactlyElementsOf(priorityQueue);
+    }
+
+    static Stream<Arguments> IntegerListWithListToRemovePriorityHeapDataProvider() {
+        return Stream.of(
+                Arguments.of(List.of(4, 2, 6, 12, 9, 2, 61, 1), List.of(4, 2)),
+                Arguments.of(List.of(12, 822, 2, 143, 1, 32, 61, 111), List.of(4, 2, 822, 1)),
+                Arguments.of(List.of(-4, 0, -12, 461, 3, 32, 135, 1),
+                        List.of(-4, 0, -12, 461, 3, 32, 135, 1)));
+    }
+
+}
